d9857bce77f4185c33d4c3073a174b4f67c88ea3
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-05-02 18:54:01 +0600

    added authMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
 
 
 const app: Application = express();
@@ -11,6 +12,9 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/', (req: Request, res: Response) => {
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface AuthRequest extends Request {
+  user?: any;
+}
+
+export const protect = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  let token;
+
+  if (req.headers.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token failed' });
+  }
+};
